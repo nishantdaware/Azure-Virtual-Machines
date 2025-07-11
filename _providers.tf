acf012950375a99bd5c0cfb014d962c94aa7184a
Nishant <dawarenishant13@gmail.com>
2025-07-11 15:44:23 +0530

    Add VNET & Virtual Machines

---
+++ _providers.tf
@@ -0,0 +1,21 @@
+// Providers block can be copied from terraform registry
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+  // The features {} block is mandatory in the azurerm provider, even if it's empty. 
+  // Omitting it will cause an error like: The provider configuration is invalid
+  
+  
+  features {} 
+  resource_provider_registrations = "none"
+  subscription_id = "0cfe2870-d256-4119-b0a3-16293ac11bdc" // This is temporary subscription id
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "virtual_machine_rg" {
+    type = string
+    default = "1-07379853-playground-sandbox"
+}
+
+variable "location" {
+    type = string
+    default = "eastus"
+}
+
+variable "prefix" {
+  default = "my-first"
+}
