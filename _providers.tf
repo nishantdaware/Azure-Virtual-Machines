b0d56f0663e6718bde104ff11c50b6856f017606
Nishant Daware <dawarenishant13@gmail.com>
2025-07-12 11:30:17 +0530

    Update _providers.tf

--- _providers.tf
+++ _providers.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
   // The features {} block is mandatory in the azurerm provider, even if it's empty. 
   // Omitting it will cause an error like: The provider configuration is invalid
   
-  
   features {} 
+
   resource_provider_registrations = "none"
   subscription_id = "0cfe2870-d256-4119-b0a3-16293ac11bdc" // This is temporary subscription id
 }
